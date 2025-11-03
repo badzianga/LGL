8251c5aa2b0f198f9e5d6139a0354f0381954d78
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-03 09:47:59 +0100

    Create Image functionality using stb_image and stb_image_write

---
+++ include/image.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "surface.h"
+
+Surface ImageLoad(const char* path);
+void ImageSave(Surface image, const char* path);
---
+++ src/image.c
@@ -0,0 +1,40 @@
+#include "image.h"
+#include "color.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+Surface ImageLoad(const char* path) {
+    int format;
+    Surface surface;
+    surface.pixels = (Color*)stbi_load(
+        path,
+        &surface.width,
+        &surface.height,
+        &format,
+        sizeof(Color));
+    const Color* end = &surface.pixels[surface.width * surface.height];
+    for (Color* pixel = surface.pixels; pixel != end; ++pixel) {
+        Color new = ColorConstructRGBA(pixel->b, pixel->g, pixel->r, pixel->a);
+        *pixel = new;
+    }
+
+    return surface;
+}
+
+void ImageSave(Surface image, const char* path) {
+    const Color* end = &image.pixels[image.width * image.height];
+    for (Color* pixel = image.pixels; pixel != end; ++pixel) {
+        Color new = ColorConstructRGBA(pixel->b, pixel->g, pixel->r, pixel->a);
+        *pixel = new;
+    }
+
+    stbi_write_png(
+        path,
+        image.width,
+        image.height,
+        sizeof(Color),
+        image.pixels,
+        image.width * (int)sizeof(Color));
+}
