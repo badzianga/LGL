80d4721ee2c4ac0bb97a3c18af1bd7627d5b752a
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-10 20:28:32 +0100

    Remove unused function header from Surface.h

--- include/Surface.h
+++ include/Surface.h
@@ -13,5 +13,4 @@ Surface SurfaceCreate(int width, int height, const PixelFormat* format);
 void SurfaceDestroy(Surface* surface);
 Surface SurfaceCopy(Surface src);
 Surface SurfaceMakeConverted(Surface surface, const PixelFormat* format);
-void SurfaceFill(Surface surface, Color color);
 void SurfaceBlit(Surface dest, Surface src, int x, int y);
