065bbf0c97256ef21ab30bbf2034a4da2ff0f915
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-14 22:11:54 +0100

    Implement WindowSetTitle and time-related functions #16

--- src/platform/PlatformWindows.c
+++ src/platform/PlatformWindows.c
@@ -14,7 +14,19 @@ typedef struct Platform {
     Surface surface;
 } Platform;
 
+typedef struct TimeHandling {
+    LARGE_INTEGER freq;
+    LARGE_INTEGER startTime;
+    LARGE_INTEGER lastFrameTime;
+
+    float frameTime;
+    double time;
+    int targetFPS;
+    double targetFrameTime;
+} TimeHandling;
+
 static Platform platform = { 0 };
+static TimeHandling timeHandling = { 0 };
 
 extern int main(int argc, char** argv);
 
@@ -23,11 +35,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     return main(__argc, __argv);
 }
 
-static LRESULT CALLBACK WndProc(const HWND hwnd, const UINT uMsg, const WPARAM wParam, const LPARAM lParam) {
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CLOSE: {
             platform.shouldClose = true;
-            DestroyWindow(hwnd);
             return 0;
         }
         case WM_DESTROY: {
@@ -100,15 +111,18 @@ bool IsCursorHidden() {
 
 // --------------------------------------------------------------------------------------------------------------------
 
+static void InitTimer();
+static void FrameTick();
+
 static wchar_t* MakeWideTitle(const char* title) {
-    int len = MultiByteToWideChar(CP_UTF8, 0, title, -1, NULL, 0);
+    const int len = MultiByteToWideChar(CP_UTF8, 0, title, -1, NULL, 0);
     wchar_t* wTitle = malloc(len * sizeof(wchar_t));
     MultiByteToWideChar(CP_UTF8, 0, title, -1, wTitle, len);
     return wTitle;
 }
 
 Surface WindowInit(int width, int height, const char* title) {
-    const HINSTANCE hInstance = GetModuleHandle(NULL);
+    HINSTANCE hInstance = GetModuleHandle(NULL);
 
     const WNDCLASS wc = {
         .lpfnWndProc = WndProc,
@@ -149,11 +163,15 @@ Surface WindowInit(int width, int height, const char* title) {
     platform.bmpInfo.bmiHeader.biBitCount = 32;
     platform.bmpInfo.bmiHeader.biCompression = BI_RGB;
 
+    timeBeginPeriod(1);
+    InitTimer();
+
     return platform.surface;
 }
 
 void WindowDestroy() {
-    assert(0 && "not implemented");
+    timeEndPeriod(1);
+    DestroyWindow(platform.hwnd);
 }
 
 void WindowSetClose(bool close) {
@@ -165,7 +183,11 @@ bool WindowShouldClose() {
 }
 
 void WindowSetTitle(const char* title) {
-    assert(0 && "not implemented");
+    if (!platform.hwnd) return;
+
+    wchar_t* wTitle = MakeWideTitle(title);
+    SetWindowTextW(platform.hwnd, wTitle);
+    free(wTitle);
 }
 
 void WindowBeginFrame() {
@@ -181,7 +203,7 @@ void WindowBeginFrame() {
 }
 
 void WindowEndFrame() {
-    const HDC dc = GetDC(platform.hwnd);
+    HDC dc = GetDC(platform.hwnd);
 
     StretchDIBits(dc,
         0, 0, platform.surface.width, platform.surface.height,
@@ -193,16 +215,58 @@ void WindowEndFrame() {
     );
 
     ReleaseDC(platform.hwnd, dc);
+
+    FrameTick();
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+static void InitTimer() {
+    QueryPerformanceFrequency(&timeHandling.freq);
+    QueryPerformanceCounter(&timeHandling.startTime);
+    timeHandling.lastFrameTime = timeHandling.startTime;
+    timeHandling.time = 0.0;
+}
+
+static void FrameTick() {
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+
+    const double elapsed = (double)(now.QuadPart - timeHandling.lastFrameTime.QuadPart) / (double)timeHandling.freq.QuadPart;
+    if (timeHandling.targetFPS > 0 && elapsed < timeHandling.targetFrameTime) {
+        const double remaining = timeHandling.targetFrameTime - elapsed;
+
+        const DWORD ms = (DWORD)(remaining * 1000.0);
+
+        if (ms > 0) {
+            Sleep(ms);
+        }
+
+        while (true) {
+            QueryPerformanceCounter(&now);
+            const double nowElapsed =
+                (double)(now.QuadPart - timeHandling.lastFrameTime.QuadPart) / (double)timeHandling.freq.QuadPart;
+            if (nowElapsed >= timeHandling.targetFrameTime) break;
+        }
+    }
+
+    QueryPerformanceCounter(&now);
+    timeHandling.frameTime =
+        (float)((double)(now.QuadPart - timeHandling.lastFrameTime.QuadPart) / (double)timeHandling.freq.QuadPart);
+    timeHandling.lastFrameTime = now;
+
+    timeHandling.time = (double)(now.QuadPart - timeHandling.startTime.QuadPart) / (double)timeHandling.freq.QuadPart;
 }
 
 void WindowSetTargetFPS(int fps) {
-    assert(0 && "not implemented");
+    timeHandling.targetFPS = fps;
+    timeHandling.targetFrameTime = (fps > 0) ? 1.0 / (double)fps : 0.0;
 }
 
 float WindowGetFrameTime(void) {
-    assert(0 && "not implemented");
+    return timeHandling.frameTime;
 }
 
 double WindowGetTime(void) {
-    assert(0 && "not implemented");
+    return timeHandling.time;
 }
