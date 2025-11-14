f9d543cd1b4b25a31b7b6be1947b0db7f833237c
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-14 16:25:14 +0100

    Create PlatormWindows.c #16

--- src/platform/PlatformWindows.c
+++ src/platform/PlatformWindows.c
@@ -1,1 +1,108 @@
+#include <windows.h>
+
+#include "Input.h"
 #include "Window.h"
+
+typedef struct Platform {
+
+} Platform;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+bool IsKeyPressed(KeyboardKey key) {
+
+}
+
+bool IsKeyDown(KeyboardKey key) {
+
+}
+
+bool IsKeyReleased(KeyboardKey key) {
+
+}
+
+int GetMouseX() {
+
+}
+
+int GetMouseY() {
+
+}
+
+void GetMousePosition(int* x, int* y) {
+
+}
+
+int GetMouseWheelMove() {
+
+}
+
+bool IsMouseButtonPressed(MouseButton button) {
+
+}
+
+bool IsMouseButtonDown(MouseButton button) {
+
+}
+
+bool IsMouseButtonReleased(MouseButton button) {
+
+}
+
+void SetMousePosition(int x, int y) {
+
+}
+
+void CursorShow() {
+
+}
+
+void CursorHide() {
+
+}
+
+bool IsCursorHidden() {
+
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+Surface WindowInit(int width, int height, const char* title) {
+
+}
+
+void WindowDestroy() {
+
+}
+
+void WindowSetClose(bool close) {
+
+}
+
+bool WindowShouldClose() {
+
+}
+
+void WindowSetTitle(const char* title) {
+
+}
+
+void WindowBeginFrame() {
+
+}
+
+void WindowEndFrame() {
+
+}
+
+void WindowSetTargetFPS(int fps) {
+
+}
+
+float WindowGetFrameTime(void) {
+
+}
+
+double WindowGetTime(void) {
+
+}
