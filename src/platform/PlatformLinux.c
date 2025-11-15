f6fa749e98b8babfe8749aebd9969c6c7a94200f
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-16 00:41:28 +0100

    Support missing keys on Linux

--- src/platform/PlatformLinux.c
+++ src/platform/PlatformLinux.c
@@ -52,47 +52,119 @@ static MouseState mouse = { 0 };
 
 static KeyboardKey MapKeyCode(KeySym sym) {
     switch (sym) {
-        case XK_Escape:     return KEY_ESCAPE;
-        case XK_Return:     return KEY_ENTER;
-        case XK_Tab:        return KEY_TAB;
-        case XK_space:      return KEY_SPACE;
-        case XK_BackSpace:  return KEY_BACKSPACE;
+        case XK_A: case XK_a: return KEY_A;
+        case XK_B: case XK_b: return KEY_B;
+        case XK_C: case XK_c: return KEY_C;
+        case XK_D: case XK_d: return KEY_D;
+        case XK_E: case XK_e: return KEY_E;
+        case XK_F: case XK_f: return KEY_F;
+        case XK_G: case XK_g: return KEY_G;
+        case XK_H: case XK_h: return KEY_H;
+        case XK_I: case XK_i: return KEY_I;
+        case XK_J: case XK_j: return KEY_J;
+        case XK_K: case XK_k: return KEY_K;
+        case XK_L: case XK_l: return KEY_L;
+        case XK_M: case XK_m: return KEY_M;
+        case XK_N: case XK_n: return KEY_N;
+        case XK_O: case XK_o: return KEY_O;
+        case XK_P: case XK_p: return KEY_P;
+        case XK_Q: case XK_q: return KEY_Q;
+        case XK_R: case XK_r: return KEY_R;
+        case XK_S: case XK_s: return KEY_S;
+        case XK_T: case XK_t: return KEY_T;
+        case XK_U: case XK_u: return KEY_U;
+        case XK_V: case XK_v: return KEY_V;
+        case XK_W: case XK_w: return KEY_W;
+        case XK_X: case XK_x: return KEY_X;
+        case XK_Y: case XK_y: return KEY_Y;
+        case XK_Z: case XK_z: return KEY_Z;
 
-        case XK_Left:       return KEY_LEFT;
-        case XK_Right:      return KEY_RIGHT;
-        case XK_Up:         return KEY_UP;
-        case XK_Down:       return KEY_DOWN;
+        case XK_1:            return KEY_1;
+        case XK_2:            return KEY_2;
+        case XK_3:            return KEY_3;
+        case XK_4:            return KEY_4;
+        case XK_5:            return KEY_5;
+        case XK_6:            return KEY_6;
+        case XK_7:            return KEY_7;
+        case XK_8:            return KEY_8;
+        case XK_9:            return KEY_9;
+        case XK_0:            return KEY_0;
 
-        case XK_Shift_L:    return KEY_LEFT_SHIFT;
-        case XK_Shift_R:    return KEY_RIGHT_SHIFT;
-        case XK_Control_L:  return KEY_LEFT_CTRL;
-        case XK_Control_R:  return KEY_RIGHT_CTRL;
-        case XK_Alt_L:      return KEY_LEFT_ALT;
-        case XK_Alt_R:      return KEY_RIGHT_ALT;
+        case XK_Return:       return KEY_ENTER;
+        case XK_Escape:       return KEY_ESCAPE;
+        case XK_BackSpace:    return KEY_BACKSPACE;
+        case XK_Tab:          return KEY_TAB;
+        case XK_space:        return KEY_SPACE;
 
-        case XK_F1:         return KEY_F1;
-        case XK_F2:         return KEY_F2;
-        case XK_F3:         return KEY_F3;
-        case XK_F4:         return KEY_F4;
-        case XK_F5:         return KEY_F5;
-        case XK_F6:         return KEY_F6;
-        case XK_F7:         return KEY_F7;
-        case XK_F8:         return KEY_F8;
-        case XK_F9:         return KEY_F9;
-        case XK_F10:        return KEY_F10;
-        case XK_F11:        return KEY_F11;
-        case XK_F12:        return KEY_F12;
+        case XK_minus:        return KEY_MINUS;
+        case XK_equal:        return KEY_EQUAL;
+        case XK_bracketleft:  return KEY_LEFT_BRACKET;
+        case XK_bracketright: return KEY_RIGHT_BRACKET;
+        case XK_backslash:    return KEY_BACKSLASH;
+        case XK_semicolon:    return KEY_SEMICOLON;
+        case XK_apostrophe:   return KEY_APOSTROPHE;
+        case XK_grave:        return KEY_GRAVE;
+        case XK_comma:        return KEY_COMMA;
+        case XK_period:       return KEY_PERIOD;
+        case XK_slash:        return KEY_SLASH;
+
+        case XK_Caps_Lock:    return KEY_CAPS_LOCK;
+        case XK_F1:           return KEY_F1;
+        case XK_F2:           return KEY_F2;
+        case XK_F3:           return KEY_F3;
+        case XK_F4:           return KEY_F4;
+        case XK_F5:           return KEY_F5;
+        case XK_F6:           return KEY_F6;
+        case XK_F7:           return KEY_F7;
+        case XK_F8:           return KEY_F8;
+        case XK_F9:           return KEY_F9;
+        case XK_F10:          return KEY_F10;
+        case XK_F11:          return KEY_F11;
+        case XK_F12:          return KEY_F12;
+
+        case XK_Scroll_Lock:  return KEY_SCROLL_LOCK;
+        case XK_Pause:        return KEY_PAUSE;
+        case XK_Insert:       return KEY_INSERT;
+        case XK_Home:         return KEY_HOME;
+        case XK_Page_Up:      return KEY_PAGE_UP;
+        case XK_Delete:       return KEY_DELETE;
+        case XK_End:          return KEY_END;
+        case XK_Page_Down:    return KEY_PAGE_DOWN;
+
+        case XK_Right:        return KEY_RIGHT;
+        case XK_Left:         return KEY_LEFT;
+        case XK_Down:         return KEY_DOWN;
+        case XK_Up:           return KEY_UP;
+
+        case XK_Num_Lock:     return KEY_KP_NUM_LOCK;
+        case XK_KP_Divide:    return KEY_KP_SLASH;
+        case XK_KP_Multiply:  return KEY_KP_ASTERISK;
+        case XK_KP_Subtract:  return KEY_KP_MINUS;
+        case XK_KP_Add:       return KEY_KP_PLUS;
+        case XK_KP_Enter:     return KEY_KP_ENTER;
+        case XK_KP_1:         return KEY_KP_1;
+        case XK_KP_2:         return KEY_KP_2;
+        case XK_KP_3:         return KEY_KP_3;
+        case XK_KP_4:         return KEY_KP_4;
+        case XK_KP_5:         return KEY_KP_5;
+        case XK_KP_6:         return KEY_KP_6;
+        case XK_KP_7:         return KEY_KP_7;
+        case XK_KP_8:         return KEY_KP_8;
+        case XK_KP_9:         return KEY_KP_9;
+        case XK_KP_0:         return KEY_KP_0;
+        case XK_KP_Decimal:   return KEY_KP_PERIOD;
+
+        case XK_Control_L:    return KEY_LEFT_CTRL;
+        case XK_Shift_L:      return KEY_LEFT_SHIFT;
+        case XK_Alt_L:        return KEY_LEFT_ALT;
+        case XK_Super_L:      return KEY_LEFT_SUPER;
+        case XK_Control_R:    return KEY_RIGHT_CTRL;
+        case XK_Shift_R:      return KEY_RIGHT_SHIFT;
+        case XK_Alt_R:        return KEY_RIGHT_ALT;
+        case XK_Super_R:      return KEY_RIGHT_SUPER;
+
+        default:              return KEY_UNKNOWN;
     }
-
-    if (sym >= XK_a && sym <= XK_z) {
-        return (KeyboardKey)(KEY_A + (sym - XK_a));
-    }
-
-    if (sym >= XK_0 && sym <= XK_9) {
-        return (KeyboardKey)(KEY_0 + (sym - XK_0));
-    }
-
-    return KEY_UNKNOWN;
 }
 
 static MouseButton MapMouseButton(uint32_t xButton) {
