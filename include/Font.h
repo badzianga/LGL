9630dd98ee1a81d4111cfb69991c02a720581eb7
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-11-18 23:59:32 +0100

    Add headers for truetype font module #19

---
+++ include/Font.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Surface.h"
+
+typedef struct Font {
+    void* internal;
+    int size;
+} Font;
+
+Font FontLoad(const char* path, int pixelSize);
+void FontFree(Font* font);
+void DrawFontChar(Surface surface, int x, int y, char c, Font* font, Color color);
+void DrawFontText(Surface surface, int x, int y, const char* text, Font* font, Color color);
+void ShutdownFontModule();
