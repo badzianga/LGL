46bdbda437eed9f0b33cc6fe9a2574db5f208fbb
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-12-13 17:32:50 +0100

    Use platform-specific aligned allocs

--- src/Allocator.c
+++ src/Allocator.c
@@ -1,16 +1,36 @@
-#include <stdlib.h>
-
 #include "Allocator.h"
+
+#define ALIGN_UP(x, a) (((x) + ((a) - 1)) & ~((a) - 1))
+
+#ifdef ESP_PLATFORM
+#include <esp_heap_caps.h>
 
 static void* MallocWrapper(size_t bytes, void* user) {
     (void)user;
-    return malloc(bytes);
+    const int alignment = 8;
+    bytes = ALIGN_UP(bytes, alignment);
+    return heap_caps_aligned_alloc(alignment, bytes, MALLOC_CAP_8BIT);
+}
+
+static void FreeWrapper(void* ptr, void* user) {
+    (void)user;
+    heap_caps_free(ptr);
+}
+#else
+#include <stdlib.h>
+
+static void* MallocWrapper(size_t bytes, void* user) {
+    (void)user;
+    const int alignment = 16;
+    bytes = ALIGN_UP(bytes, alignment);
+    return aligned_alloc(alignment, bytes);
 }
 
 static void FreeWrapper(void* ptr, void* user) {
     (void)user;
     free(ptr);
 }
+#endif
 
 static Allocator defaultAlloc = {
     .alloc = MallocWrapper,
