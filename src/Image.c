53563e9917132844d7d979e773318ec5ee5486fc
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-12-15 20:51:14 +0100

    Implement ImageSaveBMP

--- src/Image.c
+++ src/Image.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "Allocator.h"
 #include "Error.h"
@@ -213,5 +214,59 @@ void ImageSaveBMP(Surface image, const char* path) {
         THROW_ERROR(ERR_INVALID_PARAMS);
         return;
     }
-    return;
+
+    const int width = image.width;
+    const int height = image.height;
+
+    const int bytesPP = image.format->bytesPerPixel;
+    const int bitsPP = bytesPP << 3;
+
+    const int dstStride = ((width * bytesPP + 3) & ~3);  // align to 4 bytes by applying necessary padding
+    const uint32_t imageSize = dstStride * height;
+
+    FILE* f = fopen(path, "wb");
+    if (!f) {
+        THROW_ERROR(ERR_INVALID_PARAMS);
+        return;
+    }
+
+    BitmapHeader fileHeader = { 0 };
+    fileHeader.type = BMP_FILE_TYPE;
+    fileHeader.offset = sizeof(BitmapHeader) + sizeof(BitmapInfo) + 16;
+    fileHeader.fileSize = fileHeader.offset + imageSize;
+    fwrite(&fileHeader, sizeof(fileHeader), 1, f);
+
+    BitmapInfo infoHeader = { 0 };
+    infoHeader.size = sizeof(BitmapInfo);
+    infoHeader.width = width;
+    infoHeader.height = height;
+    infoHeader.planes = 1;
+    infoHeader.bitCount = bitsPP;
+    infoHeader.compression = BI_BITFIELDS;
+    infoHeader.imageSize = imageSize;
+    infoHeader.horizontalResolution = 2835; // 72 DPI
+    infoHeader.verticalResolution = 2835;
+    fwrite(&infoHeader, sizeof(infoHeader), 1, f);
+
+    const uint32_t rMask = image.format->rMask;
+    const uint32_t gMask = image.format->gMask;
+    const uint32_t bMask = image.format->bMask;
+    const uint32_t aMask = image.format->aMask;
+
+    fwrite(&rMask, sizeof(rMask), 1, f);
+    fwrite(&gMask, sizeof(gMask), 1, f);
+    fwrite(&bMask, sizeof(bMask), 1, f);
+    fwrite(&aMask, sizeof(aMask), 1, f);
+
+    uint8_t* row = AllocatorAlloc(dstStride);
+    memset(row, 0, dstStride);
+
+    for (int y = height - 1; y >= 0; y--) {
+        const uint8_t* src = (uint8_t*)image.pixels + y * image.stride;
+        memcpy(row, src, image.width * bytesPP);
+        fwrite(row, dstStride, 1, f);
+    }
+
+    AllocatorFree(row);
+    fclose(f);
 }
